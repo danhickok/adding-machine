2593b954567eb8bc4148b23c1e2cbdf5cfb0eb8f
Dan Hickok <danhickok@gmail.com>
2022-10-17 17:34:22 -0700

    202210171734

--- AddingMachine/AddingMachine/MainForm.cs
+++ AddingMachine/AddingMachine/MainForm.cs
@@ -2,27 +2,41 @@ namespace AddingMachine
 {
     public partial class MainForm : Form
     {
+        private const int MaxTapeTextControls = 27;
+        private readonly List<Label> TapeText = new();
+
         public MainForm()
         {
             InitializeComponent();
+            PopulateTapeTextControls();
+        }
+
+        private void PopulateTapeTextControls()
+        {
+            TapeText.Add(TapeText0);
+            for (int i = 1; i < MaxTapeTextControls; ++i)
+            {
+                var label = new Label
+                {
+                    Name = "TapeText" + i,
+                    Anchor = TapeText0.Anchor,
+                    AutoSize = TapeText0.AutoSize,
+                    Font = TapeText0.Font,
+                    Location = new System.Drawing.Point(TapeText0.Location.X, TapeText0.Location.Y - 20 * i),
+                    Size = TapeText0.Size,
+                    TextAlign = TapeText0.TextAlign,
+                    Visible = true,
+                };
+
+                TapeContainer.Controls.Add(label);
+
+                TapeText.Add(label);
+            }
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            // for mockup/testing only
-            DigitBox0.Image = DigitImages.Images["Digit8c"];
-            DigitBox1.Image = DigitImages.Images["Digit8c"];
-            DigitBox2.Image = DigitImages.Images["Digit8c"];
-            DigitBox3.Image = DigitImages.Images["Digit8c"];
-            DigitBox4.Image = DigitImages.Images["Digit8c"];
-            DigitBox5.Image = DigitImages.Images["Digit8c"];
-            DigitBox6.Image = DigitImages.Images["Digit8c"];
-            DigitBox7.Image = DigitImages.Images["Digit8c"];
-            DigitBox8.Image = DigitImages.Images["Digit8c"];
-            DigitBox9.Image = DigitImages.Images["Digit8c"];
-            DigitBox10.Image = DigitImages.Images["Digit8c"];
-            DigitBox11.Image = DigitImages.Images["Digit8c"];
-            DigitBox12.Image = DigitImages.Images["Digit8c"];
+
         }
     }
 }
