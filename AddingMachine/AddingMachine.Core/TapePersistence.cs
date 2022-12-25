45923ad26b1ffc97657c6bd2f38c8d110b1e99cf
Dan Hickok <danhickok@gmail.com>
2022-12-25 09:53:49 -0800

    Persistence (wip)

---
+++ AddingMachine/AddingMachine.Core/TapePersistence.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace AddingMachine.Core
+{
+    public class TapePersistence
+    {
+        private readonly string _defaultTapePath;
+
+        public TapePersistence() : this(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+            @"\AddingMachine\DefaultTape.amt")
+        {
+        }
+
+        public TapePersistence(string defaultTapePath)
+        {
+            _defaultTapePath = defaultTapePath;
+        }
+
+        public List<TapeEntry> Load(string? tapePath = null)
+        {
+            Debug.Print(_defaultTapePath);
+            throw new NotImplementedException();
+        }
+
+        public void Save(string? tapePath = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AddingMachine/AddingMachine.Tests/TapePersistenceTests.cs
@@ -0,0 +1,61 @@
+﻿using AddingMachine.Core;
+using System.Reflection.Metadata;
+
+namespace AddingMachine.Tests
+{
+    public class TapePersistenceTests
+    {
+        const string TestFilePath = @"C:\temp\TestTape.amt";
+        const string TestDefaultPath = @"C:\temp\DefaultTape.amt";
+
+        [SetUp]
+        public void SetUp()
+        {
+            RemoveTestFiles();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            RemoveTestFiles();
+        }
+
+        private void RemoveTestFiles()
+        {
+            try
+            {
+                File.Delete(TestFilePath);
+            }
+            catch
+            {
+                // ignore
+            }
+            
+            try
+            {
+                File.Delete(TestDefaultPath);
+            }
+            catch
+            {
+                // ignore
+            }
+        }
+
+        [Test]
+        public void TapePersistenceLoadTest()
+        {
+            var tp = new TapePersistence(TestDefaultPath);
+
+            var result = tp.Load();
+            Assert.That(result, Is.Not.Null, "Initial load of nonexistent default tape resulted in a null value");
+            Assert.That(result.Count, Is.EqualTo(0), "Initial load of nonexistent default tape resulted in a non-empty list");
+        }
+
+        [Test]
+        public void TapePersistenceSaveTest()
+        {
+            var tp = new TapePersistence();
+            tp.Save();
+        }
+    }
+}
